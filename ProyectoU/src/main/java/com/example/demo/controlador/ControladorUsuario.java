3459d95cc3133090a1a3edcc929c20c2a6441ab6
EmilioSZ <emiliosz1708@gmail.com>
2024-02-26 10:46:06 -0500

    creacion de caso de uso: listar las reservas de un usuario

--- ProyectoU/src/main/java/com/example/demo/controlador/ControladorUsuario.java
+++ ProyectoU/src/main/java/com/example/demo/controlador/ControladorUsuario.java
@@ -1,5 +1,29 @@
 package com.example.demo.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.modelo.*;
+import com.example.demo.repositorio.*;
+
+@RestController
+@RequestMapping("/usuario")
 public class ControladorUsuario {
+	@Autowired
+	private repositorioUsuario repositorioUsuario;
+	
+	@Autowired
+	private repositorioReserva repositorioReserva;
+	
+	@GetMapping("/consultar_reserva/{idUsuario}")
+	public List<Reserva> consultarReservaUsuario(@PathVariable Long idUsuario){
+		return repositorioReserva.reservasPorUsuario(idUsuario);
+		
+	}
+	
 
 }
--- ProyectoU/src/main/java/com/example/demo/repositorio/repositorioReserva.java
+++ ProyectoU/src/main/java/com/example/demo/repositorio/repositorioReserva.java
@@ -1,11 +1,16 @@
 package com.example.demo.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.modelo.Reserva;
 
 public interface repositorioReserva extends JpaRepository<Reserva,Long>{
 	
-	
+	@Query(value = "SELECT * FROM reservas WHERE id_usuario = :idUsuario", nativeQuery = true)
+    public List<Reserva> reservasPorUsuario(@Param("idUsuario") Long idUsuario);
 	
 }
